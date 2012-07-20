ccf5a71567e88b215c6914a87b5dac10cbbc862c
Seiya Kawashima <skawashima@uchicago.edu>
2012-07-20 10:54:40 -0500

    Add a method called 'getLoopProperties' to calculate 'Divide and conquer' boundary

--- src/main/java/distributed/filter/AbstractEachFilter.java
+++ src/main/java/distributed/filter/AbstractEachFilter.java
@@ -13,4 +13,24 @@ public abstract class AbstractEachFilter<I, R> implements Filter<I, R> {
 		this.split = split;
 		this.numberOfFilter = numberOfFilter;
 	}
+
+	final protected int[] getLoopProperties(int initialNumberOfTasks, I[] input) {
+
+		int[] properties = new int[3];
+		if (initialNumberOfTasks == 0) {
+
+			properties[0] = input.length
+					/ Runtime.getRuntime().availableProcessors();
+			properties[1] = Runtime.getRuntime().availableProcessors();
+			properties[2] = input.length % properties[1];
+
+		} else {
+
+			properties[0] = input.length / numberOfFilter;
+			properties[1] = numberOfFilter;
+			properties[2] = input.length % numberOfFilter;
+		}
+
+		return properties;
+	}
 }
