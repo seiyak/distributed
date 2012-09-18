783d406ddc9a1f697eeec1a8e230ad00d0b75474
Seiya Kawashima <skawashima@uchicago.edu>
2012-09-18 00:16:21 -0500

    Change the scope of WHITE_SPACE from private to public

--- src/main/java/distributed/filter/impl/WhiteSpaceFilter.java
+++ src/main/java/distributed/filter/impl/WhiteSpaceFilter.java
@@ -4,7 +4,7 @@ import distributed.filter.AbstractEachFilterString;
 
 public class WhiteSpaceFilter extends AbstractEachFilterString {
 
-	private static final String WHITE_SPACE = "\\s";
+	public static final String WHITE_SPACE = "\\s";
 
 	public WhiteSpaceFilter(boolean split, int numberOfFilter) {
 		super( WHITE_SPACE, split, numberOfFilter );
